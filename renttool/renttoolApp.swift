38652e72cbfd158b0534a10032fb05deccea1caa
sapoepsilon <47342870+sapoepsilon@users.noreply.github.com>
2021-04-10 15:51:02 -0600

    Initial Commit

---
+++ renttool/renttoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  renttoolApp.swift
+//  renttool
+//
+//  Created by Ismatulla Mansurov on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct renttoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
